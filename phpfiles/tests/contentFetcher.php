e7c68cfe547292ce8c9825670bf95fadc7e31ff6
Ralph <ralphdsanon@hotmail.com>
2012-10-23 11:01:46 -0400

    Tests de requetes PHP - MediaWiki
    Quelques tests pour obtenir des infos de MediaWiki avec PHP.

---
+++ phpfiles/tests/contentFetcher.php
@@ -0,0 +1,48 @@
+﻿<?php
+	/*
+		Série de tests de requêtes (hardcoded)
+	*/
+
+
+	/*
+		Retourne les utilisateurs (ainsi que le temps où ils ont édité) jusqu'à aujourd'hui
+	*/
+	function getAllUsers($website, $article){
+		$url = $website."/w/api.php?action=query&prop=revisions&rvstart=basetimestamp&rvprop=user|timestamp&format=json&redirects&titles=".$article;
+		return file_get_contents($url); 
+	}
+	
+	/*
+		Retourne les id des revisions et leur auteur
+		revid : id de la revision
+		parentid : id de la revision qui a été revisé
+	*/
+	function getAllRevId($website, $article){
+		$url = $website."/w/api.php?action=query&prop=revisions&rvstart=basetimestamp&rvprop=user|ids&format=json&redirects&titles=".$article;
+		return file_get_contents($url); 
+	}
+	/*
+		Retourne le contenu dans les revisions
+		Désagréable à voir
+	*/
+	function getAllRevContent($website, $article){
+		$url = $website."/w/api.php?action=query&prop=revisions&rvstart=basetimestamp&rvprop=ids|content&format=json&redirects&titles=".$article;
+		return file_get_contents($url); 
+	}
+	
+	
+	/*
+		Retourne les revisions(ainsi que la date) fait par un utilisateur spécifique.
+		L'utilisateur par défaut est Sola2012 (aucune raison particulière).
+	*/
+	function getUserRevIds($website, $article, $user){
+	
+		if (empty($user)){
+			$user = "Sola2012";
+		}
+		
+		$url = $website."/w/api.php?action=query&prop=revisions&rvstart=basetimestamp&rvuser=".$user."&rvprop=user|ids|timestamp&format=json&redirects&titles=".$article;
+		return file_get_contents($url); 
+	}
+
+?>
---
+++ phpfiles/tests/tester.php
@@ -0,0 +1,23 @@
+﻿<?php
+	/*
+		Effectue une série de tests.
+	*/
+	
+	include("contentFetcher.php");
+	ini_set('user_agent', 'ProjetWiki (https://github.com/yolo-hipster/yolo-hipster; ralphdsanon@hotmail.com)'); //Requis pour éviter une erreur 403
+	
+	function testContentFetcher(){
+		$site = "http://en.wikipedia.org";
+		$article = "Celine%20Dion";
+		$skipper = "<br><br>";
+		$content = getAllUsers($site, $article);
+		$content.= $skipper;
+		$content.= getAllRevId($site, $article);
+		$content.= $skipper;
+		$content.= getUserRevIds($site, $article, "");
+		echo $content;
+	}
+	
+	testContentFetcher();
+
+?>
