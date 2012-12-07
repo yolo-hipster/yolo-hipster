883787cca6ac2eeb4a0dbfe0e60eeb70e7eb935a
Sophie Déziel <courrier@sophiedeziel.com>
2012-12-07 16:44:33 -0500

    Début de AJAX pour les résultats

---
+++ UI/quiEdit.php
@@ -0,0 +1,33 @@
+<?php
+$result = '<div class="contributeurs">
+                <h2>Nom</h2>
+            <ol>
+                <li>sophiedeziel : <span>10</span></li>
+                <li>ialexca : <span>9</span></li>
+                <li>prega : <span>7</span></li>
+                <li>rlamour : <span>6</span></li>
+                <li class="total">total : <span>32</span></li>
+            </ol>
+            </div>
+            <table>
+            <tbody>
+                <tr>
+                    <th scope="row">sophiedeziel</th>
+                    <td>31.25%</td>
+                </tr>
+                <tr>
+                    <th scope="row">ialexca</th>
+                    <td>28.125%</td>
+                </tr>
+                <tr>
+                    <th scope="row">prega</th>
+                    <td>21.875%</td>
+                </tr>
+                <tr>
+                    <th scope="row">rlamour</th>
+                    <td>18.75%</td>
+                </tr>
+            </tbody>
+        </table>';
+print $result;
+?>
